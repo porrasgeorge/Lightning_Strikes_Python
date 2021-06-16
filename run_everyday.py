d07c675ec74c53d915cabc467d97d1ecf7375b4c
Jorge Porras <porrasgeorge@gmail.com>
2021-06-16 07:52:17 -0600

    correccion -1day

--- run_everyday.py
+++ run_everyday.py
@@ -2,5 +2,5 @@ import generate_files as gf
 import datetime as dt
 ##from datetime import timedelta
 
-end_date = dt.date.today() - dt.timedelta(days=1)
+end_date = dt.date.today() ##- dt.timedelta(days=1)
 gf.generate_reports(end_date)
